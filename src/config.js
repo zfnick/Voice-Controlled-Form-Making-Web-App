6abcd3c774ee5d3dfb6615a92dbe522a8cd38e72
Nick Ooi <126893520+zfnick@users.noreply.github.com>
2024-05-30 17:05:46 +0800

    removed API key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = "973001513456-4ebl4vhck9tqr5aapook5vp9t0g6tviv.apps.googleusercontent.com";
-const API_KEY = "AIzaSyBDb535hkTTSNMBFc8e-qHKweaxNmqP4r8";
+const CLIENT_ID = "";
+const API_KEY = " ";
 
 // export both
 export { CLIENT_ID, API_KEY };
